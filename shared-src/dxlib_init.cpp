0bda5d9c9b74b6b94593166a610de43b4c9334c2
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2024-12-13 12:45:41 +0900

    Disable device reset regardless of window mode.

--- shared-src/dxlib_init.cpp
+++ shared-src/dxlib_init.cpp
@@ -16,37 +16,30 @@ SDxLibInit::SDxLibInit(void* pWindowHandle)
 	{
 		iRet = DxLib::SetUserWindow(hWnd);
 		if (iRet == -1)return;
-	}
-	iRet = DxLib::SetUserWindowMessageProcessDXLibFlag(hWnd != nullptr ? FALSE : TRUE);
-	if (iRet == -1)return;
 
-	iRet = DxLib::SetChangeScreenModeGraphicsSystemResetFlag(hWnd != nullptr ? FALSE : TRUE);
-	if (iRet == -1)return;
+		iRet = DxLib::SetUserWindowMessageProcessDXLibFlag(FALSE);
+		if (iRet == -1)return;
+	}
 
 	iRet = DxLib::ChangeWindowMode(TRUE);
 	if (iRet == -1)return;
-#endif
+
 	iRet = DxLib::SetMultiThreadFlag(TRUE);
+	if (iRet == -1)return;
+#endif
+	iRet = DxLib::SetChangeScreenModeGraphicsSystemResetFlag(FALSE);
 	if (iRet == -1)return;
 
 	iRet = DxLib::SetUseCharCodeFormat(DX_CHARCODEFORMAT_UTF8);
 	if (iRet == -1)return;
 
 	iDxLibInitialised = DxLib::DxLib_Init();
+	if (iDxLibInitialised == -1)return;
 
-	iRet = DxLib::SetDrawScreen(DX_SCREEN_BACK);
-	if (iRet == -1)
-	{
-		DxLib::DxLib_End();
-		iDxLibInitialised = -1;
-		return;
-	}
-
-	iRet = DxLib::SetDrawMode(DX_DRAWMODE_BILINEAR);
-	if (iRet == -1)return;
-
-	iRet = DxLib::SetTextureAddressMode(DX_TEXADDRESS_WRAP);
-	if (iRet == -1)return;
+	/* Not fatal even if failed. */
+	DxLib::SetDrawScreen(DX_SCREEN_BACK);
+	DxLib::SetDrawMode(DX_DRAWMODE_BILINEAR);
+	DxLib::SetTextureAddressMode(DX_TEXADDRESS_WRAP);
 }
 
 SDxLibInit::~SDxLibInit()
